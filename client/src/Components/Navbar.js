7f7ca7039a68376abe7d3bdf63da59c74ea00d1b
Mandalemula Hema Siri Ramya <hemasiri1405@gmail.com>
2026-02-23 19:08:27 +0530

    Refactor Navbar to use react-bootstrap components

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -1,94 +1,48 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-function Navbar() {
+function Navigation() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
-      <div className="container-fluid">
-        {/* Brand / Site Name */}
-        <Link className="navbar-brand fw-bold text-success" to="/">
-          RecyConnect
-        </Link>
+    <Navbar expand="lg" sticky="top" style={{ backgroundColor: "#1b5e20", padding: "12px 0" }} variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/" className="fw-bold fs-3">
+          Recy<span style={{ color: "#81c784" }}>Connect</span>
+        </Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto align-items-center">
+            {/* HOME: Scrolls to the top ID of Front.js */}
+            <Nav.Link href="#top" className="mx-2">Home</Nav.Link>
+            
+            {/* ABOUT: Scrolls to How It Works */}
+            <Nav.Link href="#how-it-works" className="mx-2">How it Works</Nav.Link>
+            
+            <NavDropdown 
+                title="Login" 
+                id="login-dropdown" 
+                className="mx-2"
+            >
+              <NavDropdown.Item as={Link} to="/user_login">As User</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/vendor_login">As Vendor</NavDropdown.Item>
+            </NavDropdown>
 
-        {/* Toggler for mobile view */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        {/* Navbar links */}
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-
-            {/* Login Dropdown */}
-            <li className="nav-item dropdown">
-              <Link
-                className="nav-link dropdown-toggle"
-                to="#"
-                id="loginDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Login
-              </Link>
-              <ul className="dropdown-menu" aria-labelledby="loginDropdown">
-                <li>
-                  <Link className="dropdown-item" to="/user_login">
-                    As User
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/vendor_login">
-                    As Vendor
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/admin_login">
-                    As Admin
-                  </Link>
-                </li>
-              </ul>
-            </li>
-
-            {/* Register Dropdown */}
-            <li className="nav-item dropdown ms-3">
-              <Link
-                className="nav-link dropdown-toggle"
-                to="#"
-                id="registerDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Register
-              </Link>
-              <ul className="dropdown-menu" aria-labelledby="registerDropdown">
-                <li>
-                  <Link className="dropdown-item" to="/user_registration">
-                    As User
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/vendor_registration">
-                    As Vendor
-                  </Link>
-                </li>
-              </ul>
-            </li>
-
-          </ul>
-        </div>
-      </div>
-    </nav>
-  )
+            {/* SIGN UP: Fixed color by removing custom bg-light class that caused white-out */}
+            <NavDropdown 
+                title={<span className="text-light">Sign Up</span>} 
+                id="signup-dropdown" 
+                className="mx-2 border border-light rounded px-2"
+            >
+              <NavDropdown.Item as={Link} to="/user_registration">As User</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/vendor_registration">As Vendor</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Navbar
+export default Navigation;
