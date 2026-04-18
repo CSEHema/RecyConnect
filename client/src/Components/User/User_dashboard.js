804d72650943d995861735ebfef94d7b71b5e102
B Bhavitha <125049350+bhavitha9052004@users.noreply.github.com>
2026-04-18 18:49:43 +0530

    Update User_dashboard.js

--- client/src/Components/User/User_dashboard.js
+++ client/src/Components/User/User_dashboard.js
@@ -23,7 +23,7 @@ import USER_PROFILE_ICON from "../../static/images/Untitled_1.png";
 import VENDOR_PROFILE_IMG from "../../static/images/Untitled_1.png";
 import jug from "../../static/images/plastic_jug.jpg";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://recyconnect.onrender.com";
 
 const User_dashboard = ({ onLogout, user }) => {
   const navigate = useNavigate();
